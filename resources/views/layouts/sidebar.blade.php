9e01bef716fe2c6862a5eae08004ec3c7d0714f0
shahiran <shahiran@zuessolution.com>
2019-03-25 14:10:06 +0800

    change to laravel blade section

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,12 +1,15 @@
 <nav id="sidebar">
     <div class="sidebar-header">
         <h3>Bootstrap Sidebar</h3>
+        <strong>BS</strong>
     </div>
 
     <ul class="list-unstyled components">
-        <p>Dummy Heading</p>
         <li class="active">
-            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-home"></i>
+                Home
+            </a>
             <ul class="collapse list-unstyled" id="homeSubmenu">
                 <li>
                     <a href="#">Home 1</a>
@@ -20,10 +23,14 @@
             </ul>
         </li>
         <li>
-            <a href="#">About</a>
-        </li>
-        <li>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+            <a href="#">
+                <i class="fas fa-briefcase"></i>
+                About
+            </a>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-copy"></i>
+                Pages
+            </a>
             <ul class="collapse list-unstyled" id="pageSubmenu">
                 <li>
                     <a href="#">Page 1</a>
@@ -37,11 +44,31 @@
             </ul>
         </li>
         <li>
-            <a href="#">Portfolio</a>
+            <a href="#">
+                <i class="fas fa-image"></i>
+                Portfolio
+            </a>
         </li>
         <li>
-            <a href="#">Contact</a>
+            <a href="#">
+                <i class="fas fa-question"></i>
+                FAQ
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fas fa-paper-plane"></i>
+                Contact
+            </a>
+        </li>
+    </ul>
+
+    <ul class="list-unstyled CTAs">
+        <li>
+            <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+        </li>
+        <li>
+            <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
         </li>
     </ul>
 </nav>
-
